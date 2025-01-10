af147b36c947382d48be2eee628a10595fb83394
Gabriel Bucsan <gabriel.bucsan@sankhya.com.br>
2025-01-10 10:01:28 -0300

    feat: reorganização do código para suportar gui

--- src/main/java/com/bucsan/Main.java
+++ src/main/java/com/bucsan/Main.java
@@ -2,37 +2,20 @@ package com.bucsan;
 
 import com.bucsan.analysis.AnalysisHelper;
 import com.bucsan.analysis.AnalysisResult;
-import com.bucsan.analysis.FileHelper;
-import com.bucsan.analysis.GovDocument;
 import com.bucsan.view.ExcelHelper;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
         String[] expressoesChave = new String[]{"aten..o b.sica", "aten..o prim.ria"};
+        String directoryPath = "./src/main/resources";
+
         AnalysisHelper analysisHelper = new AnalysisHelper();
+        List<AnalysisResult> results = analysisHelper.runAnalysis(directoryPath, expressoesChave);
         ExcelHelper excelHelper = new ExcelHelper();
-        Path dir = Paths.get("./src/main/resources");
-        List<AnalysisResult> results = new ArrayList<>();
-
-        try {
-            try (Stream<Path> paths = Files.list(dir)) {
-                paths.filter(Files::isDirectory)
-                        .forEach(folderPath -> results.add(analysisHelper.analyseFolder(folderPath, expressoesChave)));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         excelHelper.exportResultsAsXlsx(results);
 
     }
--- src/main/java/com/bucsan/analysis/AnalysisHelper.java
+++ src/main/java/com/bucsan/analysis/AnalysisHelper.java
@@ -1,17 +1,38 @@
 package com.bucsan.analysis;
 
+import com.bucsan.view.ExcelHelper;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class AnalysisHelper {
 
-    public AnalysisResult analyseFolder(Path folderPath, String[] expressoesChave) {
+    public List<AnalysisResult> runAnalysis(String directoryPath, String[] expressoesChave) {
+        AnalysisHelper analysisHelper = new AnalysisHelper();
+        List<AnalysisResult> results = new ArrayList<>();
+        Path dir = Paths.get(directoryPath);
+
+        try {
+            try (Stream<Path> paths = Files.list(dir)) {
+                paths.filter(Files::isDirectory)
+                        .forEach(folderPath -> results.add(analysisHelper.analyseFolder(folderPath, expressoesChave)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return results;
+    }
+
+    private AnalysisResult analyseFolder(Path folderPath, String[] expressoesChave) {
         FileHelper reader = new FileHelper();
         AnalysisResult result = new AnalysisResult(folderPath.getFileName().toString(), expressoesChave);
 
@@ -51,7 +72,7 @@ public class AnalysisHelper {
         return analysisHelper.hasExpressionInText(documento.getTexto(), expressoesChave);
     }
 
-    public boolean hasExpressionInText(String text, String[] expressoesChave) {
+    private boolean hasExpressionInText(String text, String[] expressoesChave) {
 
         for(String expressao : expressoesChave) {
             Pattern pattern = Pattern.compile(expressao);
